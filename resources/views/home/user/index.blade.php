8f16c5e46a0563a85e2b7268fc13968437b12061
AZIZMOH9 <aziz_moh364@yahoo.com>
2022-06-11 02:16:09 +0300

    user menu

---
+++ resources/views/home/user/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.frontbased')
+
+@section('title','hello')
+
+
+
+@section('content')
+    <div class="inner">
+        <section>
+            <h2>user menu</h2>
+            <div>
+                @include('home.user.usermenu')
+            </div>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+@endsection
---
+++ resources/views/home/user/usermenu.blade.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="/about">my profile</a></li>
+    <li><a href="/products">my order</a></li>
+    <li><a href="checkout.html">my reviews</a></li>
+    <li><a href="/referance">checkout</a></li>
+    <li><a href="/contact">my product</a></li>
+    <li><a href="/logut">log out</a></li>
+</ul>
